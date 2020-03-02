c484a85810c699f5eefa0769184ddf5d0131a359
Anton Shchokotov <58229945+antonshchokotov@users.noreply.github.com>
2020-03-02 21:36:56 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    if (/\/ 0/.test(expr)) {
+        throw new Error ('TypeError: Division by zero.');
+    }
+    let brackets = expr.split('').filter(x => x === '(' || x === ')').join('');
+    let currentLength = 0;
+    do {
+        currentLength = brackets.length;
+        brackets = brackets.replace('()', '');
+    } while (currentLength != brackets.length)
+    if (brackets.length > 0) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    }
+    var func = new Function("return " + expr);
+    return func();
 }
 
 module.exports = {
